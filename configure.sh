f0fb85d0737f46eabe9dfc702444eb5043572bb3
root <root@zappa.orion>
2017-07-14 12:53:08 -0400

    Enable and start various services.

--- configure.sh
+++ configure.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-yum install -y spamassassin mariadb mariadb-server cacti cyrus-imapd net-snmp-utils net-snmp
+yum install -y spamassassin mariadb mariadb-server cacti cyrus-imapd net-snmp-utils net-snmp nut
 
 mkdir -p /var/lib/imap/sieve/d/dhill/
 cp sieve/* /var/lib/imap/sieve/d/dhill/
 chmod 755 /var/lib/imap/sieve/d/dhill/sieve
 
 cp cyrus/* /etc/
+systemctl enable cyrus-imapd
 systemctl restart cyrus-imapd
 
 cp ssh/* /root/.ssh/
@@ -14,8 +15,15 @@ chmod 600 /root/.ssh/authorized_keys
 
 cp postfix/* /etc/postfix
 postmap /etc/postfix/header_checks
+systemctl enable postfix
+systemctl restart postfix
 
 
 cp -pr mailscanner/* /etc/MailScanner
 cp -pr mail/* /etc/mail/spamassassin/
+systemctl enable mailscanner
 systemctl restart mailscanner
+
+cp ups/* /etc/ups
+systemctl enable nut-server
+systemctl restart nut-server
