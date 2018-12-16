66e9e5e0c5478e27301416dff7a22e8c2a2fb7f4
root <root@zappa.orion>
2018-12-16 16:27:09 -0500

    Install cyrus-imapd debuginfo packages.

--- configure.sh
+++ configure.sh
@@ -25,6 +25,7 @@ function enable_start {
 }
 
 yum install -y spamassassin mariadb mariadb-server cacti cyrus-imapd net-snmp-utils net-snmp nut dhcp-server selinux-policy-devel gcc syslinux-tftpboot tftp-server tftp
+debuginfo-install cyrus-imapd
 
 wget https://www.dcc-servers.net/dcc/source/dcc.tar.Z
 tar xvf dcc.tar.Z
